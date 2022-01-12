f570e2506a483732e5329f28c9baab4f0bd2be3c
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-01-12 10:36:03 -0500

    fix renderer String.replace dollar sign bug

--- test/src/pages/index.svelte
+++ test/src/pages/index.svelte
@@ -16,7 +16,7 @@ I'm index howdy ok<br>
 <a href="/hey/" class="red">Hey</a>
 <a href="/some/page/" class="red">Some</a>
 
-<Cayo name="Test"><div>Default Content</div></Cayo>
+<Cayo name="Test" object={{ some: 'what?$staticlink$'}}><div>Default Content</div></Cayo>
 
 <Cayo name="Cool" beans='ayo'>Whaaat</Cayo>
 <Cayo name="Cool" beans='beeeeaaaaasnnss' />
