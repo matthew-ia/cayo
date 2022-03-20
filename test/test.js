b06f13474d1e7ff2f5546920260c3321e664dcfc
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-03-20 14:30:48 -0400

    wip: created classes for main objects; worked on the render flow

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ import { default as fse } from 'fs-extra';
 import yargs from 'yargs-parser';
 import path from 'path';
 
+
 async function run() {
   const argv = yargs(process.argv);
   const options = {
@@ -12,34 +13,53 @@ async function run() {
     // mode: cmd === 'build' ? 'production' : 'development',
   }
 
-  const stats = {
-    dependencies: { },
-    compiled: new Set(),
+  const _cayo = {
+    stats: {
+      dependencies: { },
+      compiled: new Set(),  
+    }
   }
+
+  // const stats = {
+  //   dependencies: { },
+  //   compiled: new Set(),
+  // }
 
   const cayoConfig = await loadConfig(options);
   // 1. Compile Cayo Component (lib/components/Cayo.svelte)
   await compile.cayoComponent(cayoConfig);
-  // const { outputPath: templateOutputPath } = await compile.layout(cayoConfig);
-  // const TemplateComponent = (await import(templateOutputPath)).default;
-  // const template = TemplateComponent.render();
+
+  const { outputPath: templateOutputPath } = await compile.layout(cayoConfig);
+  const Layout = (await import(templateOutputPath)).default;
+  _cayo.layout = await Layout.render();
   
   // 2. Compile Compile Pages (src/pages/**/*.svelte)
   //    The result includes the compiled page information (name, code content, and dependencies)
 
-  await compile.pages(null, stats, cayoConfig)
+  const pages = await compile.pages(null, _cayo, cayoConfig)
+
+  let outputPath = path.resolve(
+    cayoConfig.cayoPath, 
+    `./__cayo/pages.json`
+  );
+
+  for (const p of pages.compiledPages) {
+    p.render(false, [ 'Cool', 'Test']);
+  }
+  // console.log('pages', pages);
+  await fse.outputFile(outputPath, JSON.stringify(pages, null, 2));
 
   // TODO: change this; stats should be a formally maintained object inside the main build process
   // For now, we manually mutate the result data to include the overall build stats 
   // which includes a flat dep tree and a list of all compiled components
-  // NOTE: stats.compiled is a Set, so we need to turn it into an array for iteration
-  stats.compiled = {
-    paths: [...stats.compiled]
+  // NOTE: _cayo.stats.compiled is a Set, so we need to turn it into an array for iteration
+  _cayo.stats.compiled = {
+    paths: [..._cayo.stats.compiled]
   };
-  for (let depender in stats.dependencies) {
-    stats.dependencies[depender] = [...stats.dependencies[depender]];
+  for (let depender in _cayo.stats.dependencies) {
+    _cayo.stats.dependencies[depender] = [..._cayo.stats.dependencies[depender]];
   }
-  await fse.outputFile(path.resolve(cayoConfig.cayoPath, './__cayo/test.json'), JSON.stringify(stats, null, 2));
+  await fse.outputFile(path.resolve(cayoConfig.cayoPath, './__cayo/test.json'), JSON.stringify(_cayo.stats, null, 2));
 }
 
 run();
