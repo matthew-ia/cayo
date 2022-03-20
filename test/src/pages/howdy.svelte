7570d353d268ff9f6002e4ff54420750a458d39e
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-03-20 16:59:35 -0400

    wip: small tweaks

--- test/src/pages/howdy.svelte
+++ test/src/pages/howdy.svelte
@@ -1,15 +1,12 @@
 <script>
   import Cayo from '#cayo/component';
-  import Cool from '../components/Cool.cayo.svelte';
-  import NotCool from '../components/some/Cool.cayo.svelte';
-
-
   // console.log('howdy', Cool);
 </script>
 
 howdyyy
 <br><a href="/">Back</a>
 <Cayo src="../components/Cool.cayo.svelte" />
+<Cayo src={"../components/some/Cool.cayo.svelte"} />
 <Cayo src={"../components/some/Cool.cayo.svelte"} />
 
 <slot name="entry">
