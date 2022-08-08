db1d5547aa043f575feeeaa1c29e23066faa37eb
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-08-07 23:54:27 -0400

    fix config log; cleanup

--- lib/components/Cayo.svelte
+++ lib/components/Cayo.svelte
@@ -1,19 +1,13 @@
 <script>
-  // TODO: consider using same hash tool as Svelte
-  // import { Boundary } from '@crownframework/svelte-error-boundary';
-
-  // export let name;
   export let src;
 
   // TODO: Consider error handling for required stuff
   // if (!name) throw new Error('No name for component');
   // try {
-    // if (!name) throw new Error(`\x1b[31mCayo Component: 'name' prop is required and can't be an empty string`);
+    // if (!name) throw new Error(`Cayo Component: 'name' prop is required and can't be an empty string`);
   // } catch (err) {
   //   new Error(err);
   // }
-
-  // console.log(Component);
   
   const json = JSON.stringify($$restProps);
 </script>
