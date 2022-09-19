07308948a06668e96e79d44789667f9112f367a4
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-09-18 20:51:40 -0400

    wip: template

---
+++ template/src/components/counter.cayo.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let count = 0;
+</script>
+
+<div class="wrapper">
+  <button on:click={() => count++}>Count: {count}</button>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+  }
+  button {
+    padding: .5rem;
+    background-color: transparent;
+    border: 1px solid black;
+    border-radius: 5px;
+    transition: .2s;
+  }
+  button:hover {
+    cursor: pointer;
+    border-color: rgb(81, 95, 240);
+    background-color: rgba(81, 95, 240, .1);
+  }
+</style>
---
+++ template/src/pages/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Cayo, Entry } from 'cayo';
+  const heading = '🏝 Cayo Project';
+</script>
+
+<h1>{heading}</h1>
+<Cayo src="counter.cayo.svelte" />
+<Entry src="index.js" />
+
+<style>
+  h1 {
+    text-align: center;
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+</style>
