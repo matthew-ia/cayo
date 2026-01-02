60924916c184489c5e28dcfd0a2f81efe3781500
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2026-01-02 14:00:44 -0500

    feat: added backward compatibility for old template placeholders
    also added deprecation warning and updated docs
    
    also updated the template example

--- template/src/__template.svelte
+++ template/src/__template.svelte
@@ -3,12 +3,12 @@
     <meta charset="UTF-8" />
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏝</text></svg>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    %cayo.title%
-    %cayo.css%    
-    %cayo.head%
+    <template cayo="title"></template>
+    <template cayo="css"></template>    
+    <template cayo="head"></template>
   </head>
   <body>
-    %cayo.body%
-    %cayo.script%
+    <template cayo="body"></template>
+    <template cayo="script"></template>
   </body>
 </html>
